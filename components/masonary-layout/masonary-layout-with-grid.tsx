db295b45258c635485f792d190e7db61fa1cabd2
Kaialan <satyajit1501@gmail.com>
2025-04-28 21:12:10 +0530

    fix: image loading

--- components/masonary-layout/masonary-layout-with-grid.tsx
+++ components/masonary-layout/masonary-layout-with-grid.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import OptimizedImage from "../images/optimized-image-component";
 
 const MasonaryWithGrid = () => {
   return (
@@ -18,12 +19,12 @@ const ColumnOne = () => {
     <div className="flex flex-col gap-4">
       <Link href='/projects/h4b-s3'>
         <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-          <Image
+          <OptimizedImage
             className="w-full rounded-sm shadow select-none"
             src="https://images.ctfassets.net/xsgwjwcuxltx/4hBCJLJPHlYPFwRKnX5cBr/392a5ebf49172181a33758d2a98efd1a/h4b-s3.png"
             alt="h4b-s3"
-            width={0}
-            height={0}
+            width={1728}
+            height={1064}
           />
           <div className="w-full flex flex-col leading-4">
             <p>Hack4Bengal Season 3</p>
@@ -35,12 +36,12 @@ const ColumnOne = () => {
       </Link>
       <Link href='/projects/axtionable' >
         <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-          <Image
+          <OptimizedImage
             className="w-full rounded-sm shadow select-none"
             src="https://images.ctfassets.net/xsgwjwcuxltx/6F2Fs3xL3wSGVR7DbEyMwr/baadd87e04843f4349f15e82fa501b94/axtionable.png"
             alt="axtionable"
-            width={0}
-            height={0}
+            width={1512}
+            height={1133}
           />
           <div className="w-full flex flex-col leading-4">
             <p>Axtionable</p>
@@ -52,12 +53,12 @@ const ColumnOne = () => {
       </Link>
       <Link href='projects/ganga-faucet-catalouge'>
         <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-          <Image
+          <OptimizedImage
             className="w-full rounded-sm shadow select-none"
             src="https://images.ctfassets.net/xsgwjwcuxltx/yAJDKCtgA4gAdx8wiU5do/421cea49988eb71ccf3d14108576ecfb/ganga-bathings-catalogue.png"
             alt="h4b-s3"
-            width={0}
-            height={0}
+            width={988}
+            height={684}
           />
           <div className="w-full flex flex-col leading-4">
             <p>Catalouge Design for Gangabathfittings</p>
@@ -68,12 +69,12 @@ const ColumnOne = () => {
         </div>
       </Link>
       <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-        <Image
+        <OptimizedImage
           className="w-full rounded-sm shadow select-none"
           src="https://images.ctfassets.net/xsgwjwcuxltx/68DnOUZ6DC8qGuS1Z44ouJ/ba44231448a2328179d18bca18617dba/retro_floppydisk.png"
           alt="h4b-s3"
-          width={0}
-          height={0}
+          width={516}
+          height={661}
         />
         <div className="w-full flex flex-col leading-4">
           <p>Retro Floppy Disk</p>
@@ -84,12 +85,12 @@ const ColumnOne = () => {
       </div>
       <Link href="/projects/h4b-s4-graphics" className="w-full">
         <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-          <Image
+          <OptimizedImage
             className="w-full rounded-sm shadow select-none"
             src="https://images.ctfassets.net/xsgwjwcuxltx/1sRLXuoTkdMhuZgt8REB9N/ff76af7bc58ba979684f938634a8f4c0/h4b-s4-graphics-thumbnail.png"
             alt="h4b-s3"
-            width={0}
-            height={0}
+            width={1920}
+            height={1080}
           />
           <div className="w-full flex flex-col leading-4">
             <p>Hack4Bengal S4 Graphics</p>
@@ -101,12 +102,12 @@ const ColumnOne = () => {
       </Link>
       <Link href='/projects/pixel-graphics'>
         <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-          <Image
+          <OptimizedImage
             className="w-full rounded-sm shadow select-none"
             src="https://images.ctfassets.net/xsgwjwcuxltx/7ewLyRnP37eLmxQWqeX7HE/8f68ede76e497a4e9741a0f2b0f46f2d/Pixel-Cherry.png"
             alt="h4b-s3"
-            width={0}
-            height={0}
+            width={600}
+            height={600}
           />
           <div className="w-full flex flex-col leading-4">
             <p>Pixel Cherry</p>
@@ -117,12 +118,12 @@ const ColumnOne = () => {
         </div>
       </Link>
       <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-        <Image
+        <OptimizedImage
           className="w-full rounded-sm shadow select-none"
           src="https://images.ctfassets.net/xsgwjwcuxltx/6RFc58JgQzx3dIpjbF0pIs/4f36329e97b7ea0015d417e162d24f34/retro_game_console.png"
           alt="h4b-s3"
-          width={0}
-          height={0}
+          width={516}
+          height={661}
         />
         <div className="w-full flex flex-col leading-4">
           <p>Retro Game Console</p>
@@ -140,12 +141,12 @@ const ColumnTwo = () => {
     <div className="flex flex-col gap-4">
       <Link href="/projects/sparkit" className="w-full">
         <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-          <Image
+          <OptimizedImage
             className="w-full rounded-sm shadow select-none"
             src="https://images.ctfassets.net/xsgwjwcuxltx/79bVlYaTQfaS8K98cj3i9p/2f504b66a3aac8070d5d301381bcce17/sparkit-homepage.png"
             alt="h4b-s3"
-            width={0}
-            height={0}
+            width={1508}
+            height={1131}
           />
           <div className="w-full flex flex-col leading-4">
             <p>Sparkit Webdesign</p>
@@ -157,12 +158,12 @@ const ColumnTwo = () => {
       </Link>
       <Link href='/projects/h4b-s4' >
         <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-          <Image
+          <OptimizedImage
             className="w-full rounded-sm shadow select-none"
             src="https://images.ctfassets.net/xsgwjwcuxltx/4IPyGSuqOA0cdpDX7ZjwKq/c417b599218f2ec5a7ac27360a407956/h4b-s4.png"
             alt="h4b-s3"
-            width={0}
-            height={0}
+            width={1728}
+            height={1123}
           />
           <div className="w-full flex flex-col leading-4">
             <p>Hack4Bengal Season 4</p>
@@ -174,12 +175,12 @@ const ColumnTwo = () => {
       </Link>
       <Link href='/projects/90s-art-ui'>
         <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-          <Image
+          <OptimizedImage
             className="w-full rounded-sm shadow select-none"
             src="https://images.ctfassets.net/xsgwjwcuxltx/6jBJHDTXwIn6ZnEax2rY4C/fa7694184df9c39f29fbd1964507578f/90-s_Pixel_Art.png"
             alt="h4b-s3"
-            width={0}
-            height={0}
+            width={542}
+            height={377}
           />
           <div className="w-full flex flex-col leading-4">
             <p>90s Pixel Art Ui</p>
@@ -191,12 +192,12 @@ const ColumnTwo = () => {
       </Link>
       <Link href='https://centered-checkout-573080.framer.app/' >
         <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-          <Image
+          <OptimizedImage
             className="w-full rounded-sm shadow select-none"
             src="https://images.ctfassets.net/xsgwjwcuxltx/5gl3RLfnddrK6eMskwfizS/4d751c68b71f81a7fcf12b338d079b0e/portfolio-himesh.png"
             alt="h4b-s3"
-            width={0}
-            height={0}
+            width={1302}
+            height={756}
           />
           <div className="w-full flex flex-col leading-4">
             <p>Portfolio Himesh Mehta</p>
@@ -207,12 +208,12 @@ const ColumnTwo = () => {
         </div>
       </Link>
       <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-        <Image
+        <OptimizedImage
           className="w-full rounded-sm shadow select-none"
           src="https://images.ctfassets.net/xsgwjwcuxltx/2iY8Ky3rJuMR3uHgzBGr7I/ecc18ac19f48ad5ed0661cfef9e40075/lisa_poster_-_1.png"
           alt="h4b-s3"
-          width={0}
-          height={0}
+          width={958}
+          height={1552}
         />
         <div className="w-full flex flex-col leading-4">
           <p>Lisa Poster</p>
@@ -222,12 +223,12 @@ const ColumnTwo = () => {
         </div>
       </div>
       <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-        <Image
+        <OptimizedImage
           className="w-full rounded-sm shadow select-none"
           src="https://images.ctfassets.net/xsgwjwcuxltx/bhR1gYEkDGq8skbDsqE2J/af1abdac5f90bb67bce9beea62c7eb72/azuren-1.png"
           alt="azuren"
-          width={0}
-          height={0}
+          width={736}
+          height={1103}
         />
         <div className="w-full flex flex-col leading-4">
           <p>Azuren Character Card</p>
@@ -246,12 +247,12 @@ const ColumnThree = () => {
     <div className="flex flex-col gap-4">
       <Link href='/projects/pixel-graphics'>
         <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-          <Image
+          <OptimizedImage
             className="w-full rounded-sm shadow select-none"
             src="https://images.ctfassets.net/xsgwjwcuxltx/diaRfjWLEctETFe8gWNal/a56b9bc618ee098c8efefd6573bc4b30/Pixel-Cat.png"
             alt="h4b-s3"
-            width={0}
-            height={0}
+            width={600}
+            height={600}
           />
           <div className="w-full flex flex-col leading-4">
             <p>Pixel Cat</p>
@@ -263,12 +264,12 @@ const ColumnThree = () => {
       </Link>
       <Link href="/projects/h4b-s4-graphics" className="w-full">
         <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-          <Image
+          <OptimizedImage
             className="w-full rounded-sm shadow select-none"
             src="https://images.ctfassets.net/xsgwjwcuxltx/3YNHKCmQffEts38NPa3kaZ/171a2b99e7c4770d91f724e508f920d9/h4b-4.0-kickoff.png"
             alt="h4b-s3"
-            width={0}
-            height={0}
+            width={1080}
+            height={1350}
           />
           <div className="w-full flex flex-col leading-4">
             <p>Hack4Bengal Season 4 Kick Off poster</p>
@@ -280,12 +281,12 @@ const ColumnThree = () => {
       </Link>
       <Link href='projects/portfolio-sharon'>
         <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-          <Image
+          <OptimizedImage
             className="w-full rounded-sm shadow select-none"
             src="https://images.ctfassets.net/xsgwjwcuxltx/2OKBAWY6HLtf9k0B2zDWQL/7e0c3da5526f8ddffa55d8e12d623dcd/portfolio-sharon.png"
             alt="h4b-s3"
-            width={0}
-            height={0}
+            width={1512}
+            height={984}
           />
           <div className="w-full flex flex-col leading-4">
             <p>Portfolio Sharon</p>
@@ -296,12 +297,12 @@ const ColumnThree = () => {
         </div>
       </Link>
       <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-        <Image
+        <OptimizedImage
           className="w-full rounded-sm shadow select-none"
           src="https://images.ctfassets.net/xsgwjwcuxltx/3MXtNgAhumjoGCGRD16jZn/1dd0f777b9333aaa5e5899e0ca10fafd/exdat_-_1.png"
           alt="h4b-s3"
-          width={0}
-          height={0}
+          width={1264}
+          height={982}
         />
         <div className="w-full flex flex-col leading-4">
           <p>Graphics Poster</p>
@@ -312,12 +313,12 @@ const ColumnThree = () => {
       </div>
       <Link href='/projects/chit-chat-app'>
         <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-          <Image
+          <OptimizedImage
             className="w-full rounded-sm shadow select-none"
             src="https://images.ctfassets.net/xsgwjwcuxltx/tO71XK8XdO3b5BoRI4y9j/d76731f5024bf0ecd498c7720904d751/chit-chat.png"
             alt="h4b-s3"
-            width={0}
-            height={0}
+            width={1508}
+            height={1072}
           />
           <div className="w-full flex flex-col leading-4">
             <p>Chit Chat WebApp</p>
@@ -328,12 +329,12 @@ const ColumnThree = () => {
         </div>
       </Link>
       <div className="flex flex-col gap-2 text-[14px] font-medium  pb-2">
-        <Image
+        <OptimizedImage
           className="w-full rounded-sm shadow select-none"
           src="https://images.ctfassets.net/xsgwjwcuxltx/38YHsv47Wu2cTh8FAdjZgb/56f89bbc746312c05c9687bd69bdb23b/azuren-2.png"
           alt="azuren"
-          width={0}
-          height={0}
+          width={736}
+          height={1103}
         />
         <div className="w-full flex flex-col leading-4">
           <p>Azuren Character Card 2</p>
