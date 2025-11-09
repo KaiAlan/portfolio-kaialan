24c230246514e758ba27ceb322c3f587f47361df
Kaialan <satyajit1501@gmail.com>
2025-11-10 05:10:59 +0530

    hehe

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,3 +1,4 @@
+import RotatingCD from "@/components/rotating-cd";
 import Image from "next/image";
 import React from "react";
 
@@ -20,13 +21,17 @@ const right = [
 const AboutMe = () => {
   return (
     <section className="relative w-full min-h-screen sm:min-h-[calc(100%-64px)] h-full p-5 flex flex-col justify-between items-start gap-8 overflow-hidden">
-      <Image
-        src='/red-cd.svg'
-        alt='red-cd'
+      {/* <Image
+        src="/red-cd.svg"
+        alt="red-cd"
         width={100}
         height={100}
-        className="absolute bottom-full -left-32 sm:-bottom-32 sm:-left-56 2xl:-bottom-48 2xl:-left-96 w-[300px] sm:w-[600px] 2xl:w-[1000px] -z-1"
-      />
+        className="absolute bottom-full -left-16 sm:-bottom-32 sm:-left-56 2xl:-bottom-48 2xl:-left-96 w-[300px] sm:w-[600px] 2xl:w-[1000px] -z-1"
+      /> */}
+
+      <RotatingCD className="sm:top-[70%] sm:-left-32" />
+
+      {/* <RotatingCD /> */}
       <div className="w-full flex flex-col gap-20 mt-96 sm:mt-0">
         <div className="w-full flex sm:justify-between items-start mb-10 gap-10">
           <h1 className="font-semibold">About Me</h1>
@@ -37,7 +42,15 @@ const AboutMe = () => {
           </p>
         </div>
         <div className="w-full flex justify-end items-start mb-10">
-          <p></p>
+          <p className="w-4/5 max-w-xl xl:max-w-2xl text-sm text-black text-left font-space-grotesk">
+            Hi, Im Satya(kaialan). Professionally a designer since 2023, i had a
+            creative inkling since my childhood from painting to art and craft,
+            desing has always fascinated me with joy, while i loved coding but i
+            never felt the freedom of creativity thorugh it then i found the
+            vast world of UIUX and fell in love with designing. afterwards
+            worked with seniors, mentors from our communities, learnt and grown,
+            well still learning and exploring this world full of creativity.
+          </p>
         </div>
       </div>
       <div className="w-full flex justify-end items-end mb-10">
---
+++ components/rotating-cd.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useRef, useState } from "react";
+
+interface RotatingCDProps {
+  className?: string;
+  width?: number;
+  height?: number;
+}
+
+const RotatingCD: React.FC<RotatingCDProps> = ({ className, width, height }) => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleToggle = () => {
+    if (!audioRef.current) return;
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  return (
+    <div
+      className={cn(
+        "flex gap-6 items-center justify-center absolute -left-40 -top-20 origin-center",
+        className
+      )}
+    >
+      <button
+        aria-label={isPlaying ? "Pause Song" : "Play Song"}
+        onClick={handleToggle}
+        className="focus:outline-none"
+      >
+        <img
+          src="/red-cd.png"
+          alt="red-cd"
+          width={width || 500}
+          height={height || 500}
+          className={
+            isPlaying
+              ? "animate-spin-slow origin-center rotate-45"
+              : "origin-center rotate-45"
+          }
+        />
+      </button>
+      <audio
+        ref={audioRef}
+        src="https://assets.ctfassets.net/xsgwjwcuxltx/31w204f0ZUIYd1Y1fYYElt/850b09d92f741e7b2f02d1dc024801df/cant-help-falling-in-love-lyrics.mp3"
+        loop
+      />
+    </div>
+  );
+};
+
+export default RotatingCD;
